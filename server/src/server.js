b0f96b5dcc00b45b3862403a9846cc282941104b
Aakanshi Malik <aakanshimalik54@gmail.com>
2026-06-26 18:37:38 +0530

    Resolved signup errors

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://interviewlens-ai.netlify.app' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
